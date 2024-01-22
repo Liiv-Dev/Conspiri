3282f3a8183494080c06cc40929f95b77cb43cc3
Willie Jones <williejones432@yahoo.com>
2024-01-22 17:34:29 -0600

    Added background and Login Form to LoginHbs file

--- developer/controllers/homeRoute.js
+++ developer/controllers/homeRoute.js
@@ -3,6 +3,11 @@ const router = require('express').Router(); // import express router
 router.get('/', async (req, res) => { 
     // Send the rendered Handlebars.js template back as the response
     res.render('homepage') // render homepage
+});
+
+router.get('/login', async (req, res) => { 
+    // Send the rendered Handlebars.js template back as the response
+    res.render('login') // render login page
 })
 
 module.exports = router; // export router
