c899ac2d06ddb1c24ed6847776593f5d8b27b3be
Willie Jones <williejones432@yahoo.com>
2024-01-31 10:39:00 -0600

    Updated user routes. post and comment routes being tested before push

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router(); // import express router
 const homeRoutes = require('./homeRoute'); // import homeRoute
+const apiRoutes = require('./api'); // import apiRoutes
 
 router.use('/', homeRoutes); // use homeRoute
+router.use('/api', apiRoutes); // use apiRoutes
 
 module.exports = router; // export router
